669be81bf74e4150af29f58dd27f8c36dd4912b3
bringerofmyth <melihgurgah@gmail.com>
2014-05-21 01:49:03 +0300

    asd

--- CaemAndroid/src/caemandroid/http/HttpUtility.java
+++ CaemAndroid/src/caemandroid/http/HttpUtility.java
@@ -1,9 +1,92 @@
 package caemandroid.http;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.os.AsyncTask;
+
 public class HttpUtility {
 
-	public static void postRequest (){
+	public static void postRequest (String url, List<NameValuePair> pairs, HttpConnectionObject httpResponse)
+	{
+		 HttpClient httpclient = new DefaultHttpClient();
+		    HttpPost httppost = new HttpPost(url);
+
+		    try {
+		        // Add your data
+		        /*List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		        nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+		        nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));*/
+		    	
+		        httppost.setEntity(new UrlEncodedFormEntity(pairs));
+
+		        // Execute HTTP Post Request
+		        HttpResponse response = httpclient.execute(httppost);
+		        int code = response.getStatusLine().getStatusCode();
+		        if (httpResponse == null){
+		        	httpResponse = new HttpConnectionObject();
+		        }
+		        httpResponse.setResponseCode(code);
+		        
+		    } catch (ClientProtocolException e) {
+		        // TODO Auto-generated catch block
+		    } catch (IOException e) {
+		        // TODO Auto-generated catch block
+		    }
 		
 	}
 	
+	private class MyAsyncTask extends AsyncTask<String, Integer, Double>{
+		 
+		@Override
+		protected Double doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			postData(params[0]);
+			return null;
+		}
+ 
+		protected void onPostExecute(Double result){
+			//pb.setVisibility(View.GONE); progress bar
+			//Toast.makeText(getApplicationContext(), "command sent", Toast.LENGTH_LONG).show();
+		}
+		protected void onProgressUpdate(Integer... progress){
+			//pb.setProgress(progress[0]);
+		}
+ 
+		public void postData(String valueIWantToSend) {
+			// Create a new HttpClient and Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("http://somewebsite.com/receiver.php");
+ 
+			try {
+				// Add your data
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+				nameValuePairs.add(new BasicNameValuePair("myHttpData", valueIWantToSend));
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+ 
+				// Execute HTTP Post Request
+				HttpResponse response = httpclient.execute(httppost);
+ 
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+		}
+ 
+	}
+	
+	
+	
 }
+
