468cdbc156db55b58ec694467590fbef2e2a798c
bringerofmyth <melihgurgah@gmail.com>
2014-05-22 18:03:45 +0300

    aaa

--- CaemAndroid/src/caemandroid/activities/MainActivity.java
+++ CaemAndroid/src/caemandroid/activities/MainActivity.java
@@ -1,9 +1,31 @@
 package caemandroid.activities;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import caemandroid.entity.User;
+import caemandroid.http.HttpConnectionObject;
+import caemandroid.http.HttpUtility;
+
 import com.example.caemandroid.R;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -13,11 +35,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+@SuppressLint("ShowToast")
 public class MainActivity extends Activity {
 
 	private String array_spinner[];
 	private Button login, register;
 	private EditText userName , pass;
+	private String userNameStr="", passStr ="" ;
+	private JSONObject jsonObject = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +55,7 @@ public class MainActivity extends Activity {
         ArrayAdapter adapter = new ArrayAdapter(this,
         android.R.layout.simple_spinner_item, array_spinner);
         s.setAdapter(adapter);
-        
+ 
         userName = (EditText) findViewById(R.id.mUsernameEdit);
         pass = (EditText) findViewById(R.id.mPasswordEdit);
         
@@ -62,7 +87,12 @@ public class MainActivity extends Activity {
     			|| pass.getText().toString().equals("")  ){
     		return false;
     	}
-    	return true;
+    	else{
+    		userNameStr = userName.getText().toString();
+    		passStr = pass.getText().toString();
+    		return true;
+    	}
+    	
     	
     }
 	protected void toLoginActivity() {
@@ -70,12 +100,69 @@ public class MainActivity extends Activity {
 			Toast.makeText(this, "Please fill all fields.",Toast.LENGTH_SHORT ).show();
 		}
 		else{
-			
+			new loginAsyncTask("Yükleniyor").execute();
 		}
 		
 	}
+	@SuppressLint("NewApi")
+	private class loginAsyncTask extends AsyncTask<Void, Void, Void> {
+		String modalMesaj;
+		ProgressDialog dialog;
+		String productName = "";
+		List<NameValuePair> pairs = new ArrayList <NameValuePair>(2);
 
+        public loginAsyncTask(String mMesaj) {
+            this.modalMesaj = mMesaj;
+            this.dialog = new ProgressDialog(MainActivity.this);
+        }
 
+        @Override
+        protected void onPreExecute() {
+            
+        	pairs.add(new BasicNameValuePair("Username", userNameStr));
+        	pairs.add(new BasicNameValuePair("Password", passStr));
+        	dialog.setMessage(modalMesaj);
+            dialog.setIndeterminate(true);
+            dialog.setCancelable(false);
+            dialog.show();
+            
+        }
+        
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			HttpUtility.createPostRequest(HttpUtility.POST_USER_URL, pairs);
+			return null;
+		}
+		 @Override
+	        protected void onPostExecute(Void str) {
+	 
+	            if (dialog.isShowing())
+	                dialog.dismiss();
+	            try {
+		           	 
+	            	if(jsonObject !=null && jsonObject.getString("Username")!=null && !jsonObject.getString("Username").isEmpty() ){
+	            		HttpUtility.passedJson = jsonObject;
+	            		HttpUtility.createIntent(MainActivity.this, WelcomeScreenActivity.class );
+
+		            	return;
+	            	}
+	            	HttpUtility.toastMessage(MainActivity.this, "Username/password combination is not matched.");
+	            	
+	               // String id = jsonObject.getString("Telefon").toString();
+	              
+	                	                
+	 
+	            } catch (JSONException e) {
+	            	HttpUtility.toastMessage(MainActivity.this, "Exception:Username/password combination is not matched.");
+	                e.printStackTrace();
+	            }
+	 
+	           // text.setText(responseBody);
+	 
+	        }
+	    }
+	
 
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ CaemAndroid/src/caemandroid/activities/WelcomeScreenActivity.java
@@ -0,0 +1,26 @@
+package caemandroid.activities;
+
+import com.example.caemandroid.R;
+import com.example.caemandroid.R.layout;
+import com.example.caemandroid.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class WelcomeScreenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome_screen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.welcome_screen, menu);
+		return true;
+	}
+
+}
